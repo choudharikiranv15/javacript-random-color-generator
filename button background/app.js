d493e63f457efb1abaa9f369f56c97c26343e16b
KIRAN CHOUDHARI <choudharikiranv2003@gmail.com>
2024-02-17 13:09:36 +0530

    color added

--- button background/app.js
+++ button background/app.js
@@ -8,5 +8,6 @@ const buttons = document.querySelectorAll('button');
 for (let button of buttons){
     button.addEventListener('click',function(){
         button.style.backgroundColor = makeRandColor();
+        button.style.color= makeRandColor();
     })
 }
